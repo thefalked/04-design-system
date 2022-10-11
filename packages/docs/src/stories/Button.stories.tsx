385caa4c62e0a820cdad7beced84576024c7c4ce
thefalked <giulianocrivelli@hotmail.com>
2022-10-10 21:30:38 -0400

    feat: adding action to Button on StoryBook

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -9,6 +9,11 @@ export default {
   args: {
     children: "Send",
   },
+  argTypes: {
+    onClick: {
+      action: 'click'
+    }
+  }
 } as Meta;
 
 export const Primary: StoryObj<ButtonProps> = {};
